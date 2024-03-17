72454cbfaec20502240ab17de4886598a74aa801
Shiroku-ma <y.sinpei0221@gmail.com>
2024-03-17 22:40:27 +0900

    fix: Improve solving equations.

--- equation.py
+++ equation.py
@@ -2,69 +2,59 @@ from term import Term
 from expression import Expression as Expr
 import math
 import cmath
-from fractions import Fraction as F
+from fractions import Fraction as Fr
 
 class IllegalEquationException(Exception):
     pass
 
 class Solution:
-    def __init__(self, re_ra=F(0), re_ir=[F(0),0], im_ra=F(0), im_ir=[F(0),0]):
-        self.re_ra = re_ra
-        self.re_ir = re_ir
-        self.im_ra = im_ra
-        self.im_ir = im_ir
-    
+    def __init__(self, body, is_imag=False):
+        self.body = body
+        self.is_imag = is_imag
+
     def __str__(self) -> str:
-        result = ""
-        if self.re_ra != 0:
-            c = self.re_ra
-            if c == 1:
-                c = ""
-            elif c == -1:
-                c = "-"
-            result += str(c)
-        if self.re_ir[0] != 0 and self.re_ir[1] != 0:
-            if result != "":
-                result += "+"
-            outside = self.re_ir[0]
-            inside = self.re_ir[1]
-            if inside == 1:
-                if outside == 1:
-                    result += "1"
-                else:
-                    result += str(outside)
-            elif outside == 1:
-                result += "√" + str(inside)
-            else:
-                result += str(outside) + "√" + str(inside)
-        if self.im_ra != 0:
-            if result != "":
-                result += "+"
-            c = self.im_ra
-            if c == 1:
-                c = ""
-            elif c == -1:
-                c = "-"
+        parts = []
+        if self.body[0] != 0:
+            parts.append(str(self.body[0]))
 
-            result += str(c) + "i"
-        if self.im_ir[0] != 0 and self.im_ir[1] != 0:
-            if result != "":
-                result += "+"
-            outside = self.im_ir[0]
-            inside = self.im_ir[1]
-            if inside == 1:
-                if outside == 1:
-                    result += "i"
+        if not self.is_imag:
+            if self.body[1] != 0 and self.body[2] != 0:
+                a = str(self.body[1])
+                b = str(self.body[2])
+                if b == "1":
+                    if self.body[0] == 0:
+                        parts.append(a)
+                    else:
+                        parts.clear()
+                        parts.append(str(Fr(self.body[0]) + Fr(a)))
                 else:
-                    result += str(outside) + "i"
-            elif outside == 1:
-                result += "√" + str(inside) + "i"
+                    if a == "1":
+                        parts.append("√"+b)
+                    elif a == "-1":
+                        parts.append("-√"+b)
+                    else:
+                        parts.append(a+"√"+b)
+        else:
+            a = str(self.body[1])
+            b = str(self.body[2])
+            if b == "1":
+                if a == "1":
+                    parts.append("i")
+                elif a == "-1":
+                    parts.append("-i")
+                else:
+                    parts.append(a+"i")
             else:
-                result += str(outside) + "√" + str(inside) + "i"
-        if result == "":
-            result = 0
-
-        return result
+                if a == "1":
+                    parts.append("√"+b+"i")
+                elif a == "-1":
+                    parts.append("-√"+b+"i")
+                else:
+                    parts.append(a+"√"+b+"i")
+        
+        if len(parts) == 0:
+            return "0"
+        return " + ".join(parts)
 
 class Equation:
     def __init__(self, left: Expr, right: Expr):
@@ -76,39 +66,25 @@ class Equation:
         expr.simplify()
         if expr.terms[0].exp == 1:
             [a,b] = expr.coefs()
-            return [Solution(F(-b, a))]
+            return [ Solution([Fr(-b,a), 0, 0]) ]
         elif expr.terms[0].exp == 2:
             [a,b,c] = expr.coefs()
             D = b**2-4*a*c
-            x1 = []
-            x2 = []
-            print(a,b,c)
-
-            if D == 0:
-                return [Solution(F(-b,2*a))]
-            if self.__isRrational(D):
-                d2 = math.floor(math.sqrt(abs(D)))
-                if D < 0:
-                    x1 = Solution(F(-b,2*a), [F(0),0], F(d2, 2*a), [F(0),0])
-                    x2 = Solution(F(-b,2*a), [F(0),0], -F(d2, 2*a), [F(0),0])
-                else:
-                    x1 = Solution(F(-b+d2,2*a))
-                    x2 = Solution(F(-b-d2,2*a))
+            if D > 0:
+                [p,q] = self.__format_root(D)
+                return [ Solution([Fr(-b,2*a), Fr(p,2*a), q]), Solution([Fr(-b,2*a), -Fr(p,2*a), q]) ]
+            elif D == 0:
+                return [ Solution([Fr(-b,2*a), 0, 0]) ]
             else:
-                root = self.__format_root(D)
-                if D < 0:
-                    x1 = Solution(F(-b,2*a), [F(0),0], [F(0),0], [F(root[0], 2*a), root[1]])
-                    x2 = Solution(F(-b,2*a), [F(0),0], [F(0),0], [-F(root[0], 2*a), root[1]])
-                else:
-                    x1 = Solution(F(-b,2*a), [F(root[0], 2*a), root[1]])
-                    x2 = Solution(F(-b,2*a), [-F(root[0], 2*a), root[1]])
-            return [x1, x2]
+                [p,q] = self.__format_root(-D)
+                return [ Solution([Fr(-b,2*a), Fr(p,2*a), q], True), Solution([Fr(-b,2*a), -Fr(p,2*a), q], True) ]
+            
         
     def __isRrational(self, n):
         return math.sqrt(abs(n)).is_integer()
     
     def __format_root(self, n):
-        N = abs(n)
+        N = n
         primes = []
         while N % 2 == 0:
             primes.append(2)
